c5121df3ad9ab86aef02865e2307dfdbadd5ced8
seongpil0948 <seongpil0948@gmail.com>
2020-10-06 17:59:00 +0900

    hmm

---
+++ src/bubble.ts
@@ -0,0 +1,26 @@
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
+
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+const card: HTMLDivElement = document.createElement('div');
+card.className = 'card';
+card.innerHTML = greeter(user)
+document.body.append(card)
